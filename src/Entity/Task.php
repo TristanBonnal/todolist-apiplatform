4bffc2fd4679feb1db2d885183902b7712f9f78d
TristanBonnal <bonnal.tristan@hotmail.fr>
2023-01-15 13:54:20 +0100

    groups et operations

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -3,26 +3,61 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [       // Comportement spécifique à chaque opération
+        new Get(normalizationContext: ['groups' => ['read:item']]),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Delete(denormalizationContext: ['groups' => ['read:item']])
+    ],
+    normalizationContext: [     // Données récupérées lors de la réponse
+        'groups' => ['read:collection']
+    ],
+    denormalizationContext: [       // Données persistées lors de la requête
+        'groups' => ['write:collection']
+    ]
+
+)]
 class Task
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['read:collection','read:item'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 2000)]
+    #[Groups(['read:collection', 'write:collection','read:item'])]
     private ?string $content = null;
 
     #[ORM\Column]
+    #[Groups(['write:collection','read:item'])]
     private ?bool $status = null;
 
     #[ORM\ManyToOne(inversedBy: 'task')]
+    #[Groups(['write:collection','read:item'])]
     private ?Category $category = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['write:collection','read:item', 'read:collection'])]
+    private ?\DateTimeInterface $createdAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
 
     public function getId(): ?int
     {
@@ -61,6 +96,18 @@ class Task
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
 
         return $this;
     }
