dc8330a54f2e936119fdb5a805deafb1888d82bb
TristanB <tbonnal@qerys.com>
2023-02-19 18:43:20 +0100

    update api platform

---
+++ src/Entity/Dependency.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection()
+    ],
+    paginationEnabled: false
+)]
+class Dependency
+{
+    #[ApiProperty(identifier: true)]
+    private $uuid;
+
+    #[ApiProperty(description: 'Le nom de la dépendance')]
+    private $name;
+
+    #[ApiProperty(description: 'La version de la dépendance')]
+    private $version;
+
+    public function __construct($uuid, $name, $version)
+    {
+        $this->uuid = $uuid;
+        $this->name = $name;
+        $this->version = $version;
+    }
+
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getVersion()
+    {
+        return $this->version;
+    }
+
+    public function setUuid($uuid)
+    {
+        $this->uuid = $uuid;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setVersion($version)
+    {
+        $this->version = $version;
+    }
+
+    public function __toString()
+    {
+        return $this->name . ' ' . $this->version;
+    }
+}
