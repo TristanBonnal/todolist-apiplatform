50959e070b854297ab748be6063482c7921305dc
TristanB <tbonnal@qerys.com>
2023-02-26 20:28:23 +0100

    fix stateprovider

--- src/State/DependencyStateProvider.php
+++ src/State/DependencyStateProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\State;
 
+use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\Entity\Dependency;
@@ -11,9 +12,9 @@ class DependencyStateProvider implements ProviderInterface
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
         if ($operation instanceof CollectionOperationInterface) {
-            return [new BlogPost(), new BlogPost()];
+            return [new Dependency('333', 'test', '1.0.0'), new Dependency('333', 'test', '1.0.0')];
         }
 
-        return [new Dependency('12315', 'test', '1.0.0'), new Dependency('12315', 'test', '1.0.0')];
+        return [new Dependency('12315', 'test', '1.0.0')];
     }
 }
