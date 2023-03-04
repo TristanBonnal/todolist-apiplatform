b20a989d98c46a4a1278faafab3174fab8c5abe0
TristanBonnal <bonnal.tristan@hotmail.fr>
2023-03-04 17:29:39 +0100

    data provider

--- src/State/DependencyStateProvider.php
+++ src/State/DependencyStateProvider.php
@@ -6,15 +6,44 @@ use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\Entity\Dependency;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class DependencyStateProvider implements ProviderInterface
 {
+
+    public function __construct(private readonly KernelInterface $appKernel)
+    {
+    }
+
+
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
+        // Récupération des dépendances du projet dans le fichier composer.json
+        $rootPath = $this->appKernel->getProjectDir();
+        $composerEntries = json_decode(file_get_contents($rootPath . '/composer.json'), true);
+        $dependencies = [];
+
+        // On crée un objet Dependency pour chaque dépendance
+        foreach ($composerEntries['require'] as $dependency => $version) {
+            $dependencies[] = new Dependency(
+                Uuid::uuid5(Uuid::NAMESPACE_URL, $dependency)->toString(),
+                $dependency,
+                $version
+            );
+        }
+        // Si on est sur une opération de collection, on retourne tout
         if ($operation instanceof CollectionOperationInterface) {
-            return [new Dependency('333', 'test', '1.0.0'), new Dependency('333', 'test', '1.0.0')];
+            return [$dependencies];
         }
 
-        return [new Dependency('12315', 'test', '1.0.0')];
+        // Sinon, on retourne l'élément demandé s'il existe
+        foreach ($dependencies as $dependency) {
+            if ($dependency->getUuid() === $uriVariables['uuid']) {
+                return $dependency;
+            }
+        }
+
+        return null;
     }
 }
